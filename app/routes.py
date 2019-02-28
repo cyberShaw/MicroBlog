0587dd3cd0e128a2f5a03938afdae6880dcbfe1a
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-02-28 20:29:53 +0530

    Add Templates and some Modularity :3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Harsha'}
+    posts = [
+        {
+            'author' : {'username' : 'John'},
+            'body' : 'Beautiful Day in Portland!'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body' : 'The Avengers are so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
